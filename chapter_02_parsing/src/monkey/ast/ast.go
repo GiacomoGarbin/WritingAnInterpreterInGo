c007df1f8acee2da2603132e2ef851af54662590
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2021-01-09 23:12:47 +0000

    2.6 parsing expressions : preparing the AST

--- chapter_02_parsing/src/monkey/ast/ast.go
+++ chapter_02_parsing/src/monkey/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -28,10 +32,22 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
 }
+
+func (i *Identifier) String() string { return i.Value }
 
 func (i *Identifier) ExpressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
@@ -45,6 +61,21 @@ type LetStatement struct {
 func (ls *LetStatement) StatementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string{
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String() + " = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 type ReturnStatement struct {
 	Token token.Token
 	Value Expression
@@ -52,3 +83,32 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) StatementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string{
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.Value != nil {
+		out.WriteString(rs.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) StatementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string{
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
